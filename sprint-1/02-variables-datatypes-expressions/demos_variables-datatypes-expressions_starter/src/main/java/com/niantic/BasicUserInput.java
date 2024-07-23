188a7da9db19c364b87f31043b778429bfe84f1f
Jessy Cesena <jessikah.cesena@gmail.com>
2024-07-23 14:46:37 -0700

    Finished demo

--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/BasicUserInput.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/BasicUserInput.java
@@ -4,6 +4,23 @@ public class BasicUserInput
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello World!");
+
+        int quantity;
+        quantity = 150;
+        System.out.println(quantity);
+
+        long milesToPluto = Integer.MAX_VALUE;
+        System.out.println(milesToPluto);
+        milesToPluto = milesToPluto * 2;
+        System.out.println(milesToPluto);
+
+        long age = 5000;
+        int smallAge = (int)age;
+
+
+
+
 
     }
 }
--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
@@ -4,6 +4,21 @@ public class Calculations
 {
     public static void main(String[] args)
     {
+        // Calculate how many donuts per student
+        int numberOfStudents = 4;
+        int numberOfDonuts = 22;
+
+
+        int donutsPerStudent = numberOfDonuts / numberOfStudents;
+        int remainingDonuts = numberOfDonuts % numberOfStudents;
+        double donutsPerStudentDouble = (double)numberOfDonuts / numberOfStudents;
+
+        System.out.println("Number of donuts per student");
+        System.out.println("Total donuts:" + numberOfDonuts);
+        System.out.println("Total students:" + numberOfStudents);
+        System.out.println("Donuts per student:" + donutsPerStudentDouble);
+        System.out.println();
+        System.out.println("Donuts per student (double):" + donutsPerStudentDouble);
 
     }
 }
