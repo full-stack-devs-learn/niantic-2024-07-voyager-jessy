400df3494d2dc24e2da519c4a52c2a53fa8638ff
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-05 16:47:02 -0700

    WIP Encapsulation Challenges

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
@@ -2,4 +2,90 @@ package com.niantic.part_3_challenge;
 
 public class Refrigerator
 {
+    private int currentTemperature;
+    private int maxCapacity;
+    private final int MIN_CAPACITY = 0;
+    private int defaultTemperature = 32;
+    private int capacity;
+    private int availableCapacity;
+   private int removeCapacity;
+   private int addedItem;
+
+
+    private boolean isDoorOpen = false;
+
+    public Refrigerator(int currentTemperature, int maxCapacity)
+    {
+        this.currentTemperature = currentTemperature;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getCurrentTemperature()
+    {
+        return  currentTemperature;
+    }
+
+    public int getMaxCapacity()
+    {
+        return maxCapacity;
+    }
+    public boolean isDoorOpen()
+    {
+        return isDoorOpen;
+    }
+
+    public void openDoor()
+    {
+        currentTemperature = defaultTemperature;
+        isDoorOpen = true;
+    }
+
+    public void closeDoor()
+    {
+        isDoorOpen = false;
+    }
+
+    public int getCapacity()
+    {
+        return  capacity;
+    }
+
+    public int getAvailableCapacity()
+    {
+        return maxCapacity - capacity;
+    }
+
+    public int getAddedItem()
+    {
+        return addedItem;
+    }
+
+    public void addItem(int capacity)
+    {
+        if (capacity < maxCapacity && isDoorOpen)
+        {
+            availableCapacity=+ addedItem;
+        }
+        if (capacity>= maxCapacity && isDoorOpen)
+        {
+            availableCapacity = 0;
+        }  //questionable
+    }
+
+    public int getRemoveCapacity()
+    {
+        return removeCapacity;
+    }
+
+    public void removeItem(int capacity)
+    {
+        if(isDoorOpen() && capacity>= MIN_CAPACITY)
+        {
+         availableCapacity= capacity - availableCapacity - removeCapacity;
+        }
+    }
+
+
 }
+
+
