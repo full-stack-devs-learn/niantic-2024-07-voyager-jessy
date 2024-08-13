41672766e4fa543e5def7de464964611d2f048cf
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-13 14:57:34 -0700

    Auto stash before merge of "main" and "origin/main"

---
+++ sprint-1/17-thymeleaf-part-1/demo_sakila_website/src/main/java/com/niantic/DemoSakilaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.niantic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // marks this as a springboot application
+public class DemoSakilaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSakilaWebsiteApplication.class, args);
+	}
+
+}
---
+++ sprint-1/17-thymeleaf-part-1/demo_sakila_website/src/main/java/com/niantic/controllers/ActorsControllers.java
@@ -0,0 +1,25 @@
+package com.niantic.controllers;
+
+import com.niantic.services.ActorDao;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import com.niantic.models.Actor;
+
+import java.util.ArrayList;
+
+@Controller //needs to have this so that it know this application is available
+public class ActorsControllers
+{
+    //create a function
+
+    private ActorDao actorsDao = new ActorDao();
+
+
+
+
+
+
+
+
+}
