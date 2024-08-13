4da0a061c639dec2bbcdc66bb89c5f7b2d0b7e44
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-13 14:49:26 -0700

    thymeleaf

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,26 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+
+
+import javax.swing.*;
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
-    CategoryDao categoryDao = new CategoryDao();
+    private CategoryDao categoryDao = new CategoryDao();
+
+    @getMapping("/category")
+
+    public String getCategory(Model model)
+    {
+        ArrayList< Category> categories =categoryDao.getCategories();
+       model.addAttribute("category", categories);
+
+    }
 }
