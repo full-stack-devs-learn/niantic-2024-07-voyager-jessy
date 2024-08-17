3a09c16a1fbfabfe3bdd96c1e6eb3baef4e128ba
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-17 15:13:16 -0700

    Code Cleanup

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
@@ -1,29 +1,37 @@
-package com.niantic.controllers;
-
-import com.niantic.models.Product;
-import com.niantic.services.ProductDao;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.ArrayList;
-
-
-//@Controller
-public class ProductsController {}
+//package com.niantic.controllers;
+//
+//import com.niantic.models.Product;
+//import com.niantic.services.ProductDao;
+//import com.niantic.models.Category;
+//import com.niantic.services.CategoryDao;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
+//
+//
+////@Controller
+//public class ProductsController {
 //
 //    private ProductDao productDao = new ProductDao();
+//    private CategoryDao categoryDao = new CategoryDao();
+//
 //
 ////        use http:localhost:8080/products
 //
-//    @GetMapping("/products?catId=1")
+//    @GetMapping("/products/{id}")
 //    public String getProductsByCategory(Model model, @PathVariable int id) {
-//        var product = productDao.getProductsByCategory(id);
-//        model.addAttribute("product", product);
-//        model.addAttribute("category", product);
+//        ArrayList<Category> categories = categoryDao.getCategories();
+//        ArrayList<Product> products = productDao.getProductsByCategory(id);
+//
+//        model.addAttribute("products", products);
+//        model.addAttribute("categories", categories);
 //
 //        return "/products/index";
 //    }
-//}
+//
+//
 //}
