294de9376c5bdc32ab1090ba624be9d49e82a9d8
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-23 16:12:17 -0700

    Card Game - Card Test added
    I think I will need to add more tests somewhere, but I'm not sure yet. At least this is a good start to ensure one of my biggest functions works.

--- sprint-2/04-collections/exercise_card-game/src/test/java/com/niantic/models/CardTest.java
+++ sprint-2/04-collections/exercise_card-game/src/test/java/com/niantic/models/CardTest.java
@@ -1,8 +1,28 @@
 package com.niantic.models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest
 {
+    private Card current;
+
+    @BeforeEach
+    public void setup() { current = new Card("J", "Spades");}
+
+    @Test
+    public void card_shouldReturn_FaceValue()
+    {
+        //arrange
+        int expected = 11;
+        //act
+        int actual = current.getCardFaceValue();
+
+        //assert
+        assertEquals(expected, actual, "Because the face value of J is 11.");
+
+    }
 
 }
