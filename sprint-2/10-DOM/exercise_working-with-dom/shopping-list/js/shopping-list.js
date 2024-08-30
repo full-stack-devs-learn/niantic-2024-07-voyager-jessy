ae202cc77eaa261981401d86d9cdfe673e6e5fb9
Jessy Cesena <jessikah.cesena@gmail.com>
2024-08-30 16:17:09 -0700

    DOM exercises - challenge

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,17 +1,84 @@
 // add pageTitle
 
 // add groceries
-
+const service = new ShoppingService();
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    const shoppingTitle = document.getElementById("title");
+    shoppingTitle.textContent = service.getListName();
+
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+    const lists = service.getShoppingList()
+                        // .sort((need,done) => !need.markCompleted - done.markCompleted);
+
+    const listContainer = document.getElementById("groceries");
+
+    lists.forEach(list => {
+        createListDiv(list, listContainer)
+    });
+
+}
+
+/**
+ * i added this one to create a new row for each line item, will go 
+/back to figure out how to do this within the display grogeries function 
+as soon as I am confident I can get this working
+*/
+function createListDiv(list, parent){
+
+    //create new variable to rep the new line item we are making for each item on the shopping list
+    const listDiv = document.createElement("div");
+
+    //add each item to this new class list
+    listDiv.classList.add("groceries");
+
+    parent.appendChild(listDiv);
+
+    //add all the inner divs here 
+    buildItemTitle(list, parent);
+    
+
+}
+
+function buildItemTitle(list, parent){
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add("list-item");
+    titleDiv.textContent = list.title;
+
+    parent.appendChild(titleDiv);
+
+    buildItemQuantity(list, titleDiv);
+
+}
+
+function buildItemQuantity(list, parent){
+    const quantityDiv = document.createElement('div');
+    quantityDiv.classList.add("quantity-container");
+
+
+    const quantityLabel = document.createElement('span');
+    quantityLabel.classList.add('super');
+    quantityLabel.textContent = "quantity";
+    quantityDiv.appendChild(quantityLabel);
+
+    const quantityTitle = document.createTextNode(list.quantity);
+    quantityDiv.appendChild(quantityTitle);
+    document.body.appendChild(quantityDiv);
+    
+
+    
+    parent.appendChild(quantityDiv);
+
+}
+
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
@@ -20,7 +87,13 @@ function displayGroceries() {}
 function markCompleted() {
 }
 
+document.addEventListener("DOMContentLoaded" , () => {
 
-displayListTitle();
-displayGroceries();
+    displayListTitle();
+    displayGroceries();
+
+
+
+})
+
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,21 +1,76 @@
 class ShoppingService {
 
     getListName() {
+        return 'Jessy\'s Shopping List';
     }
 
     getShoppingList() {
         return [
             {
                 id: 1,
-                title: '5 lb sack of potatoes',
+                title: '5 lb sack of mixed baby potatoes',
                 quantity: 1,
                 isComplete: false
             },
             {
                 id: 2,
-                title: 'Frozen Pizza',
+                title: 'Chicken Wings - 4 lbs',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 3,
+                title: 'Cosmic Crisp Apples',
                 quantity: 4,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Ground Coffee - Colombia 1.5lb',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 5,
+                title: 'pint Half & Half',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: 'Organic broccoli crowns',
+                quantity: 4,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: '#36 Large eggs',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Pita bread',
+                quantity: 2,
                 isComplete: true
+            },
+            {
+                id: 9,
+                title: 'Oikos protein yogurt - singles',
+                quantity: 10,
+                isComplete: true
+            },
+            {
+                id: 10,
+                title: 'Habanero peppers',
+                quantity: 9,
+                isComplete: false
+            },
+            {
+                id: 11,
+                title: 'unbleached flour 1lb',
+                quantity: 4,
+                isComplete: false
             }
         ];
     }
