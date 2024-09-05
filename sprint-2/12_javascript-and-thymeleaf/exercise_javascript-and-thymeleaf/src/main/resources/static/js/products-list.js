dae15f83ef2d73531c9707e82c68c7116f5e4820
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-04 20:16:33 -0700

    WIP - products-list.js
    Need to figure out why my buttons aren't working

---
+++ sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/resources/static/js/products-list.js
@@ -0,0 +1,132 @@
+let pageNumber = 1;
+let totalPages = 1;
+let currentPage;
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+//identify all my page buttons
+const buttons = document.getElementById("pagination");
+const first = document.getElementById("firstPage");
+const previous = document.getElementById("previous");
+const one = document.getElementById("page1");
+const two = document.getElementById("page2");
+const next = document.getElementById("next");
+const last = document.getElementById("lastPage");
+
+//add classes to buttons for beautification
+first.classList.add("page-link");
+previous.classList.add("page-link");
+one.classList.add("page-link");
+two.classList.add("page-link");
+next.classList.add("page-link");
+last.classList.add("page-link");
+
+//button logic eventListener
+first.addEventListener("click", () => {
+    if(pageNumber > 1) {
+        pageNumber= 1
+        loadPage();
+    }
+
+})
+
+previous.addEventListener("click", () => {
+    if(pageNumber > 1) {
+        pageNumber--;
+        loadPage();
+
+    }
+
+})
+
+one.addEventListener("click", () =>{
+    if(pageNumber > 1) {
+            pageNumber= 1
+            loadPage();
+        }
+})
+
+two.addEventListener("click", () =>{
+    if(pageNumber !== 2) {
+    pageNumber = 2;
+    loadPage();
+    }
+})
+
+next.addEventListener("click", () =>{
+    if(pageNumber < totalPages) {
+        pageNumber++;
+        loadPage();
+    }
+})
+
+last.addEventListener("click", () =>{
+    if(pageNumber < totalPages) {
+        pageNumber = totalPages
+        loadPage();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+//if
+
+//add class active to the page that is currently being displayed
+
+
+
+
+
+fetch(`api/products/pages`)
+    .then(response => response.text())
+    .then(data => {
+    totalPages = +data});
+//    updatePageBtn();
+
+loadPage();
+})
+
+function loadPage(){
+
+    const categoryOption = document.getElementById("category");
+    categoryOption.addEventListener("change", () =>{
+
+        const container = document.getElementById("products-container");
+        container.innerHTML = "";
+
+        const catId = categoryOption.value;
+        const url = `/products/category/${catId}`;
+
+
+        fetch(url).then(response => {
+            if(response.status === 200)
+            {
+                return response.text();
+            }
+            throw new Error(response);
+                }).then(data => {
+                    container.innerHTML = data;
+                    updatePageBtn;
+                }).catch(error => {
+                    console.log("Error in fetching products:", error)
+                });
+            })
+
+}
+
+function updatePageBtn(){
+const buttons = document.querySelectorAll(".page-link");
+buttons.forEach(btn => btn.classList.remove("active")
+//reset all pages to nonactive
+
+
+}
