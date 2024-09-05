147eb6c897240e61f0e5196952932e9b2aac582c
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-04 20:12:36 -0700

    More work on API for actual use in getting page totals

--- sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/java/com/niantic/controllers/apis/ProductsApiController.java
+++ sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/java/com/niantic/controllers/apis/ProductsApiController.java
@@ -1,2 +1,31 @@
-package com.niantic.controllers.apis;public class ProductsAPIController {
+package com.niantic.controllers.apis;
+
+import com.niantic.models.Product;
+import com.niantic.services.CategoryDao;
+import com.niantic.services.ProductDao;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class ProductsApiController {
+
+    private ProductDao productDao = new ProductDao();
+
+    @GetMapping("/api/products/pages")
+    public int getProductCountPerCategory(int categoryId)
+    {
+        int totalCount = productDao.getProductCount(categoryId);
+        int pages = totalCount/ 10;
+        if(totalCount % 10 >0){
+            pages++;
+        };
+
+        return pages;
+
+    }
+
+
+
 }
