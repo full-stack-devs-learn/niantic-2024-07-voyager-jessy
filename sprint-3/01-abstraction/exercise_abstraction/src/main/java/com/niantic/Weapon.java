b6072b2cdc73b364fe1537b1507ca24a74e96334
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-11 16:59:44 -0700

    Weapon Abstraction Exercise

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
@@ -1,5 +1,37 @@
 package com.niantic;
 
-public class Weapon
+public abstract class Weapon
 {
+    protected String name;
+    private int damage;
+    protected int percentCharged = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getPercentCharged() {
+        return percentCharged;
+    }
+
+    protected void setPercentCharged(int percentCharged) {
+        this.percentCharged = percentCharged;
+    }
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+
+    }
+
+    public abstract int attack();
+
+    public abstract int powerAttack();
+
+    public abstract int getRange();
+
 }
