6d5e3d9e492dd9fb60a0677de81afd34a012ba05
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-12 14:34:52 -0700

    Dagger Complete

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
@@ -1,5 +1,54 @@
 package com.niantic;
 
-public class Dagger
+public class Dagger extends Weapon
 {
+    private int daggerCount = 1;
+    int charge;
+
+    public Dagger(String name, int damage) {
+        super(name, damage);
+    }
+
+    public int getDaggerCount() {
+        return daggerCount;
+    }
+
+    public void addDagger(){
+        daggerCount++;
+    }
+
+
+    @Override
+    public int attack() {
+        int damageDone = 0;
+
+        if(daggerCount == 0){
+            damageDone = 0;
+            charge = getPercentCharged();
+            setPercentCharged(charge);
+        }
+        else{
+            charge = this.getPercentCharged() + 20;
+            setPercentCharged(charge);
+            damageDone =  getDamage();
+        }
+        return damageDone;
+    }
+
+    @Override
+    public int powerAttack() {
+        int damageDealt = 0;
+
+        if (getPercentCharged() == 100) {
+            damageDealt = getDamage() * 3;
+            daggerCount--;
+
+        }
+        return damageDealt;
+    }
+
+    @Override
+    public int getRange() {
+        return 10;
+    }
 }
