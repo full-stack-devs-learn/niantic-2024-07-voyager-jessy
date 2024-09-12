0700fe6681f7f3fbfd07c868fe34f45c2e2268e3
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-11 18:00:57 -0700

    Sword - powerAttack(), getRange()
    not sure if the range on powerAttack was correct in readMe file, so I did change it. But may need to alter back to <50, 50 - 90, 100

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
@@ -1,5 +1,51 @@
 package com.niantic;
 
-public class Sword
+public class Sword extends Weapon
 {
+
+    public Sword(String name, int damage) {
+        super(name, damage);
+    }
+
+    @Override
+    public int attack() {
+        int damageDealt = getDamage();
+
+        int currentPercentCharged = this.getPercentCharged();
+
+        if(currentPercentCharged > 100){
+             currentPercentCharged = 100;
+        }
+        else{
+            currentPercentCharged += 10;
+        }
+
+        setPercentCharged(currentPercentCharged);
+
+        return damageDealt;
+
+    }
+
+    @Override
+    public int powerAttack() {
+        int damageDealt;
+
+        if(this.percentCharged < 50){
+           damageDealt =  getDamage();
+        }
+        else if(this.percentCharged > 51 && this.percentCharged <= 99){
+            damageDealt = getDamage() * 2;
+            setPercentCharged(getPercentCharged() - 50);
+        }
+        else{
+            damageDealt = getDamage() * 4;
+                    setPercentCharged(0);
+        }
+        return damageDealt;
+    }
+
+    @Override
+    public int getRange() {
+        return 1;
+    }
 }
