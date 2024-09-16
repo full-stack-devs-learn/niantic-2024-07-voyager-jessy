b4e657854a0d551a70714c4d63dd68986e75c185
Jessy Cesena <jessikah.cesena@gmail.com>
2024-09-16 13:05:39 -0700

    WIP Bow - Abstraction project

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Bow.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Bow.java
@@ -1,5 +1,29 @@
 package com.niantic;
 
-public class Bow
+public class Bow extends Weapon
 {
+    private String arrowType;
+    private int quiverSize;
+    private int arrowCount;
+
+    public Bow(String name, int damage, String arrowType, int quiverSize) {
+        super(name, damage);
+        this.arrowType = arrowType;
+        this.quiverSize = quiverSize;
+    }
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public int powerAttack() {
+        return 0;
+    }
+
+    @Override
+    public int getRange() {
+        return 0;
+    }
 }
